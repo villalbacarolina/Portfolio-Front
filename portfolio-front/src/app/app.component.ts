a207e263164d26d526e86b05d21e903442c3de33
Carolina Villalba <carolinaangelesvillalba@gmail.com>
2023-11-23 02:22:11 -0300

    Se creó la base del componente education

--- portfolio-front/src/app/app.component.ts
+++ portfolio-front/src/app/app.component.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from "./home/home.component";
 import { AboutMeComponent } from './about-me/about-me.component';
+import { EducationComponent } from './education/education.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, HomeComponent, AboutMeComponent]
+    imports: [CommonModule, RouterOutlet, HomeComponent, AboutMeComponent, EducationComponent]
 })
 export class AppComponent {
   title = 'portfolio-front';
