89769fbe635e0995151482e3d840120ba9956aa7
Carolina Villalba <carolinaangelesvillalba@gmail.com>
2023-11-23 02:05:45 -0300

    Se creó la base del componente about-me

--- portfolio-front/src/app/app.component.ts
+++ portfolio-front/src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from "./home/home.component";
+import { AboutMeComponent } from './about-me/about-me.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, HomeComponent]
+    imports: [CommonModule, RouterOutlet, HomeComponent, AboutMeComponent]
 })
 export class AppComponent {
   title = 'portfolio-front';
