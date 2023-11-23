e3ace6eb8aa8a4b5b36433ede9c2dc7a9fdfefce
Carolina Villalba <carolinaangelesvillalba@gmail.com>
2023-11-23 01:50:10 -0300

    Se creó la base del componente home

--- portfolio-front/src/app/app.component.ts
+++ portfolio-front/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from "./home/home.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet]
+    imports: [CommonModule, RouterOutlet, HomeComponent]
 })
 export class AppComponent {
   title = 'portfolio-front';
