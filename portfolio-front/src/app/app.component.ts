0d7f345486c1dba2a17f6b0086d566d2df34c934
Carolina Villalba <carolinaangelesvillalba@gmail.com>
2023-11-23 02:58:56 -0300

    Se finalizó la base del componente experience

--- portfolio-front/src/app/app.component.ts
+++ portfolio-front/src/app/app.component.ts
@@ -4,13 +4,14 @@ import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from "./home/home.component";
 import { AboutMeComponent } from './about-me/about-me.component';
 import { EducationComponent } from './education/education.component';
+import { ExperienceComponent } from "./experience/experience.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, HomeComponent, AboutMeComponent, EducationComponent]
+    imports: [CommonModule, RouterOutlet, HomeComponent, AboutMeComponent, EducationComponent, ExperienceComponent]
 })
 export class AppComponent {
   title = 'portfolio-front';
